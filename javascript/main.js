d4c4710bc006fd54a0279e3e009a9cc97d2deeb4
Sim Yi Lin <simyilin@umich.edu>
2019-12-28 23:07:11 -0500

    Changed contacts page and added recent activities

--- javascript/main.js
+++ javascript/main.js
@@ -1,37 +1,37 @@
 // JavaScript source code
 
-function revealActivity(activity) {
-  if (activity == "web") {
-    document.getElementById("web").style.backgroundColor = "#ffffff80";
-    document.getElementById("webImage").style.display = "inline-block";
-    document.getElementById("webText").innerHTML =
-      "Creating my personal website";
-  } else if (activity == "german") {
-    document.getElementById("german").style.backgroundColor = "#ffffff80";
-    document.getElementById("germanImage").style.display = "inline-block";
-    document.getElementById("germanText").innerHTML = "Learning German";
-  } else if (activity == "dance") {
-    document.getElementById("dance").style.backgroundColor = "#ffffff80";
-    document.getElementById("danceImage").style.display = "inline-block";
-    document.getElementById("danceText").innerHTML = "Danceeeee!";
-  }
-}
+// function revealActivity(activity) {
+//   if (activity == "web") {
+//     document.getElementById("web").style.backgroundColor = "#ffffff80";
+//     document.getElementById("webImage").style.display = "inline-block";
+//     document.getElementById("webText").innerHTML =
+//       "Creating my personal website";
+//   } else if (activity == "german") {
+//     document.getElementById("german").style.backgroundColor = "#ffffff80";
+//     document.getElementById("germanImage").style.display = "inline-block";
+//     document.getElementById("germanText").innerHTML = "Learning German";
+//   } else if (activity == "dance") {
+//     document.getElementById("dance").style.backgroundColor = "#ffffff80";
+//     document.getElementById("danceImage").style.display = "inline-block";
+//     document.getElementById("danceText").innerHTML = "Danceeeee!";
+//   }
+// }
 
-function removeActivity(activity) {
-  if (activity == "web") {
-    document.getElementById("web").style.backgroundColor = "transparent";
-    document.getElementById("webImage").style.display = "block";
-    document.getElementById("webText").innerHTML = "";
-  } else if (activity == "german") {
-    document.getElementById("german").style.backgroundColor = "transparent";
-    document.getElementById("germanImage").style.display = "block";
-    document.getElementById("germanText").innerHTML = "";
-  } else if (activity == "dance") {
-    document.getElementById("dance").style.backgroundColor = "transparent";
-    document.getElementById("danceImage").style.display = "block";
-    document.getElementById("danceText").innerHTML = "";
-  }
-}
+// function removeActivity(activity) {
+//   if (activity == "web") {
+//     document.getElementById("web").style.backgroundColor = "transparent";
+//     document.getElementById("webImage").style.display = "block";
+//     document.getElementById("webText").innerHTML = "";
+//   } else if (activity == "german") {
+//     document.getElementById("german").style.backgroundColor = "transparent";
+//     document.getElementById("germanImage").style.display = "block";
+//     document.getElementById("germanText").innerHTML = "";
+//   } else if (activity == "dance") {
+//     document.getElementById("dance").style.backgroundColor = "transparent";
+//     document.getElementById("danceImage").style.display = "block";
+//     document.getElementById("danceText").innerHTML = "";
+//   }
+// }
 
 var menuOpen = false;
 
